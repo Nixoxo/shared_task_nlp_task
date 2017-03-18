f27e7621c6c06469e2932f08ee7db014737ab571
nixoxo <nico.axtmann95@gmail.com>
2017-03-18 01:42:57 +0100

    Just a placeholder

---
+++ scripts/special_tickets.py
@@ -0,0 +1,20 @@
+
+mamma_mia_prompts = [
+"Frag: 4 Tickets für Mamma Mia",
+"Frag: ein Ticket für Mamma Mia",
+"Frag trotzdem: ein Ticket für Mamma Mia",
+"Frag: 2 Tickets für Mamma Mia",
+"Frag: 3 Tickets für Mamma Mia"]
+
+mamma_mia_key_nouns = ['ticket', 'one ticket', 'mamma', 'one', 'mamma mia', 'tickets']
+mamma_mia_key_nouns_dt  = ['a ticket']
+
+king_of_lions_prompts = [
+"Frag: 2 Tickets für König der Löwen",
+"Frag trotzdem: ein Ticket für König der Löwen",
+"Frag: 3 Tickets für König der Löwen",
+"Frag: 4 Tickets für König der Löwen",
+"Frag: ein Ticket für König der Löwen"]
+
+king_of_lions_key_nouns = ['ticket', 'one ticket', 'one', 'lion king', 'lion', 'tickets'],
+king_of_lions_key_nouns_dt = ['a ticket', 'the lion', 'the lion king']
